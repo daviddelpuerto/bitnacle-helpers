bd7a80e3ed3b525bd14798a5aba2a765c67627e6
David del Puerto <ddelpuertomartin@gmail.com>
2019-08-26 17:47:35 +0200

    Added new "extra" object to log

--- src/formats.js
+++ src/formats.js
@@ -1,17 +1,17 @@
 module.exports = {
     
-    simple: ({ time, level, req, statusCode, elapsedTime, message }) => {
+    simple: ({ time, level, req, statusCode, extra, elapsedTime, message }) => {
         const { method, endpoint, remoteAddress, id } = req;
-        const messageObject = { time, level, method, endpoint, remoteAddress, id, statusCode, elapsedTime, message };
+        const messageObject = { time, level, method, endpoint, remoteAddress, id, statusCode, ...extra, elapsedTime, message };
         return Object.values(messageObject)
-            .filter(prop => prop !== undefined)
+            .filter(prop => prop !== undefined && typeof prop !== 'object')
             .map(prop => `[${prop}]`)
             .join(' ');
     },
 
-    json: ({ time, level, req, statusCode, elapsedTime, message }) => {
+    json: ({ time, level, req, statusCode, extra, elapsedTime, message }) => {
         const { method, endpoint, remoteAddress, id } = req;
-        return JSON.stringify({ time, level, method, endpoint, remoteAddress, id, statusCode, elapsedTime, message });
+        return JSON.stringify({ time, level, method, endpoint, remoteAddress, id, statusCode, ...extra, elapsedTime, message });
     },
 
     extended: (logMessageObject) => logMessageObject,
