5e2fa5d4f6b6893e4251be773285c5fd2bbb614b
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-28 01:03:51 +0200

    Removed function formatLocaleDateString()

--- src/timer.js
+++ src/timer.js
@@ -1,26 +1,43 @@
-const formatTimeZoneOffset = function(date) {
+function getDateFormat(date) {
+    if (!(date instanceof Date)) throw new Error('date is not an instance of Date');
+
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+        .map(d => d.toString()
+        .padStart(2, '0'))
+        .join('-');
+};
+
+function getTimeFormat(date) {
+    if (!(date instanceof Date)) throw new Error('date is not an instance of Date');
+
+    return [date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds()]
+        .map((item, index) => {
+            const padSize = index === 3 ? 3 : 2;
+            return item.toString().padStart(padSize, '0');
+        })
+        .join(':'); 
+};
+
+function formatTimeZoneOffset(date) {
+    if (!(date instanceof Date)) throw new Error('date is not an instance of Date');
+
     const timeZoneOffset = date.getTimezoneOffset();
     const leadingSymbol = timeZoneOffset < 0 ? '+' : '-';
-    const timeZoneOffsetFormat = `${(timeZoneOffset/60) * (-1)}`.padStart(2, '0');
+    const timeZoneOffsetFormat = `${Math.abs((timeZoneOffset/60))}`.padStart(2, '0');
     return `${leadingSymbol}${timeZoneOffsetFormat}00`;
-}
+};
 
-const getRequestTime = function() {
+function getRequestTime() {
     const date = new Date;
+    const dateFormat = getDateFormat(date);
+    const timeFormat = getTimeFormat(date);
     const timeOffset = formatTimeZoneOffset(date);
-    const dateFormat = [date.getFullYear(), date.getMonth() + 1, date.getDate()].map(d => d.toString().padStart(2, '0')).join('-');
-    const timeFormat = [date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds()].map((item, index) => {
-        const padSize = index === 3 ? 3 : 2;
-        return item.toString().padStart(padSize, '0');
-    }).join(':'); 
     return `${dateFormat}T${timeFormat}${timeOffset}`;
-}
-
-const formatLocaleDateString = function() {
-    return new Date().toLocaleDateString().split('/').map(d => d.padStart(2, '0')).reverse().join('');
-}
+};
 
 module.exports = {
+    formatTimeZoneOffset,
+    getDateFormat,
+    getTimeFormat,
     getRequestTime,
-    formatLocaleDateString
-}
+};
