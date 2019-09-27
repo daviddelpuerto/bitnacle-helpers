6e6bfdf3c3e4ca7a833d41cdaa013f659c4f9774
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-28 01:35:22 +0200

    Ensure the status code range

--- src/levels.js
+++ src/levels.js
@@ -1,13 +1,19 @@
 const getLogLevel = function({ statusCode }) {
-    if (statusCode >= 500) {
+    const normalizedStatusCode = parseInt(statusCode);
+
+    if (normalizedStatusCode !== normalizedStatusCode) throw new Error('statusCode must be an integer');
+
+    if (normalizedStatusCode >= 500 && normalizedStatusCode < 600) {
         return 'ERROR';
-    } else if (statusCode >= 400) {
+    } else if (normalizedStatusCode >= 400 && normalizedStatusCode < 500) {
         return 'WARNING';
-    } else if (statusCode >= 100) {
+    } else if (normalizedStatusCode >= 100 && normalizedStatusCode < 400) {
         return 'INFO';
+    } else {
+        return null;
     }
-}
+};
 
 module.exports = {
     getLogLevel
-}
+};
