4b1b4ab31641d67c4e30ffe67bfb48e261e2f575
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-28 02:05:09 +0200

    Added levels object

--- src/levels.js
+++ src/levels.js
@@ -1,14 +1,20 @@
+const levels = {
+    ERROR: 'ERROR',
+    WARNING: 'WARNING',
+    INFO: 'INFO'
+};
+
 const getLogLevel = function({ statusCode }) {
     const normalizedStatusCode = parseInt(statusCode);
 
     if (normalizedStatusCode !== normalizedStatusCode) throw new Error('statusCode must be an integer');
 
     if (normalizedStatusCode >= 500 && normalizedStatusCode < 600) {
-        return 'ERROR';
+        return levels.ERROR;
     } else if (normalizedStatusCode >= 400 && normalizedStatusCode < 500) {
-        return 'WARNING';
+        return levels.WARNING;
     } else if (normalizedStatusCode >= 100 && normalizedStatusCode < 400) {
-        return 'INFO';
+        return levels.INFO;
     } else {
         throw new Error(`invalid statusCode ${statusCode}: it must be an integer between 100 and 599 both included`);
     }
