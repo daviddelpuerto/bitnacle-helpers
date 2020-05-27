60f30353569bffb36b0823f67a76de8b66aed295
David del Puerto Martín <ddelpuertomartin@gmail.com>
2020-05-28 01:18:34 +0200

    Log user-agent

--- src/formats.js
+++ src/formats.js
@@ -1,23 +1,23 @@
 module.exports = {
     
-    simple: ({ time, level, req = {}, statusCode, extra = {}, elapsedTime, message }) => {
+    simple: ({ time, level, userAgent, req = {}, statusCode, extra = {}, elapsedTime, message }) => {
         if (typeof req !== 'object') throw new Error('req must be an object');
         if (typeof extra !== 'object') throw new Error('extra must be an object');
 
         const { method, endpoint, remoteAddress, id } = req;
-        const messageObject = { time, level, method, endpoint, remoteAddress, id, statusCode, ...extra, elapsedTime, message };
+        const messageObject = { time, level, method, endpoint, remoteAddress, userAgent, id, statusCode, ...extra, elapsedTime, message };
         return Object.values(messageObject)
             .filter(prop => prop !== undefined && typeof prop !== 'object')
             .map(prop => `[${prop}]`)
             .join(' ');
     },
 
-    json: ({ time, level, req = {}, statusCode, extra = {}, elapsedTime, message }) => {
+    json: ({ time, level, userAgent, req = {}, statusCode, extra = {}, elapsedTime, message }) => {
         if (typeof req !== 'object') throw new Error('req must be an object');
         if (typeof extra !== 'object') throw new Error('extra must be an object');
         
         const { method, endpoint, remoteAddress, id } = req;
-        return JSON.stringify({ time, level, method, endpoint, remoteAddress, id, statusCode, ...extra, elapsedTime, message });
+        return JSON.stringify({ time, level, method, endpoint, remoteAddress, userAgent, id, statusCode, ...extra, elapsedTime, message });
     },
 
     extended: (logMessageObject) => JSON.stringify(logMessageObject),
