876e71c97c2e481cf1e58ef719de60ae3600577d
David del Puerto Martín <ddelpuertomartin@gmail.com>
2020-06-02 10:14:15 +0200

    Created utils modules with function to check if stream is writable

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const bitnacleFormats = require('./src/formats');
 const bitnacleLevels = require('./src/levels');
 const bitnacleTimer = require('./src/timer');
+const bitnacleUtils = require('./src/utils');
 
 module.exports = {
     bitnacleFormats,
     bitnacleLevels,
-    bitnacleTimer
+    bitnacleTimer,
+    bitnacleUtils
 }
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+const stream = require('stream');
+
+function isWritableStream(obj) {
+    return obj instanceof stream.Stream 
+        && typeof obj._write === 'function' 
+        && typeof obj._writableState === 'object';
+};
+
+module.exports = {
+    isWritableStream
+};
