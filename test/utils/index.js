ee5bf45a3dd639f1911bb0827fc8041df5f64635
David del Puerto Martín <ddelpuertomartin@gmail.com>
2020-06-10 03:42:29 +0200

    Generate sample log file during tests

--- test/utils/index.js
+++ test/utils/index.js
@@ -3,17 +3,19 @@ const { expect } = require('chai');
 const fs = require('fs');
 const utils = require('../../src/utils');
 
+const LOG_FILE = './test/sample.log';
+
+fs.writeFileSync(LOG_FILE);
+
 describe('#isWritableStream()', () => {
-  const sampleFile = './log.txt';
-  fs.writeFileSync(sampleFile);
 
   it('should return true if its a writable stream', () => {
-    const writableStream = fs.createWriteStream(sampleFile);
+    const writableStream = fs.createWriteStream(LOG_FILE);
     expect(utils.isWritableStream(writableStream)).to.be.true;
   });
 
   it('should return false if its not a writable stream', () => {
-    const readableStream = fs.createReadStream(sampleFile);
+    const readableStream = fs.createReadStream(LOG_FILE);
 
     [{}, [], 'not a stream', readableStream].forEach((param) => {
       expect(utils.isWritableStream(param)).to.be.false;
